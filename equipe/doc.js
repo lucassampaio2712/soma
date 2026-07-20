ef0d2c1560eb142e3de4752c292f51bb7cdb0598
lucassampaio2712 <lucas.sampaio27122009@gmail.com>
2026-07-19 21:42:00 -0300

    Delete equipe directory

--- equipe/doc.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-            // 1. Dispara a animação em cascata logo após o carregamento
-            const container = document.getElementById('profileContainer');
-            setTimeout(() => {
-                container.classList.add('loaded');
-            }, 50); 
-
-            
-        });
